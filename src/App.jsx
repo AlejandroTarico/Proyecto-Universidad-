23e1897bce276450fe1f35b2eff9350d5b99236d
alejoT <yo.alejo.at@gmail.com>
2024-02-28 02:43:36 -0300

    Creación de texto

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Cambiando el contenido del mensaje para probar que esta funcionando bien 
       </p>
     </>
   )
